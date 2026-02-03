1afdff8b944168231d251131a07c20a18f4027d5
rsneelam <reshma.neelam04@gmail.com>
2026-02-04 00:52:03 +0530

    initial commit 1

---
+++ src/com/resh/practice/tictactoe/Main.java
@@ -0,0 +1,13 @@
+package com.resh.practice.tictactoe;
+
+
+import com.resh.practice.tictactoe.controllers.GameController;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        GameController gameController = new GameController();
+        Scanner scanner = new Scanner(System.in);
+    }
+}
